63449210744e485cba4fcbcd679a02959b0d94f5
Ted Ross <tross@redhat.com>
2015-09-18 15:47:38 -0400

    WIP - More infrastructure for the core thread.

--- src/router_core.c
+++ src/router_core.c
@@ -31,29 +31,75 @@
  * This module owns, manages, and uses the router-link list and the address hash table
  */
 
+
+/**
+ * The following data structures are private to the core module.  They are defined
+ * here to ensure their invisibiliy outside the core.
+ */
+
+typedef struct qd_router_core_work_t {
+    DEQ_LINKS(struct qd_router_core_work_t);
+} qd_router_core_work_t;
+
+ALLOC_DECLARE(qd_router_core_work_t);
+ALLOC_DEFINE(qd_router_core_work_t);
+DEQ_DECLARE(qd_router_core_work_t, qd_router_core_work_list_t);
+
 struct qd_router_core_t {
-    qd_log_source_t *log;
-    sys_cond_t      *cond;
-    sys_mutex_t     *lock;
-    sys_thread_t    *thread;
-    bool             running;
+    qd_log_source_t            *log;
+    sys_cond_t                 *cond;
+    sys_mutex_t                *lock;
+    sys_thread_t               *thread;
+    bool                        running;
+    qd_router_core_work_list_t  work_list;
 };
+
+
+static void router_core_do_work(qd_router_core_t *core, qd_router_core_work_t *work)
+{
+}
 
 
 static void *router_core_thread(void *arg)
 {
-    qd_router_core_t *core = (qd_router_core_t*) arg;
+    qd_router_core_t           *core = (qd_router_core_t*) arg;
+    qd_router_core_work_list_t  work_list;
+    qd_router_core_work_t      *work;
 
     qd_log(core->log, QD_LOG_INFO, "Router Core thread running");
-
-    sys_mutex_lock(core->lock);
     while (core->running) {
-        sys_cond_wait(core->cond, core->lock);
         //
-        // While there's work to do, do the work with the lock not held
+        // Use the lock only to protect the condition variable and the work list
         //
+        sys_mutex_lock(core->lock);
+
+        //
+        // Block on the condition variable when there is no work to do
+        //
+        while (core->running && DEQ_IS_EMPTY(core->work_list))
+            sys_cond_wait(core->cond, core->lock);
+
+        //
+        // Move the entire work list to a private list so we can process it without
+        // holding the lock
+        //
+        DEQ_MOVE(core->work_list, work_list);
+        sys_mutex_unlock(core->lock);
+
+        //
+        // Process and free all of the work items in the list
+        //
+        work = DEQ_HEAD(work_list);
+        while (work) {
+            DEQ_REMOVE_HEAD(work_list);
+
+            if (core->running)
+                router_core_do_work(core, work);
+
+            free_qd_router_core_work_t(work);
+            work = DEQ_HEAD(work_list);
+        }
     }
-    sys_mutex_unlock(core->lock);
 
     qd_log(core->log, QD_LOG_INFO, "Router Core thread exited");
     return 0;
@@ -63,7 +109,7 @@ static void *router_core_thread(void *arg)
 qd_router_core_t *qd_router_core(void)
 {
     qd_router_core_t *core = NEW(qd_router_core_t);
-    memset(core, 0, sizeof(qd_router_core_t));
+    ZERO(core);
 
     //
     // Set up the logging source for the router core
@@ -76,6 +122,7 @@ qd_router_core_t *qd_router_core(void)
     core->cond    = sys_cond();
     core->lock    = sys_mutex();
     core->running = true;
+    DEQ_INIT(core->work_list);
     core->thread  = sys_thread(router_core_thread, core);
 
     return core;
@@ -84,9 +131,16 @@ qd_router_core_t *qd_router_core(void)
 
 void qd_router_core_free(qd_router_core_t *core)
 {
+    //
+    // Stop and join the thread
+    //
     core->running = false;
     sys_cond_signal(core->cond);
     sys_thread_join(core->thread);
+
+    //
+    // Free the core resources
+    //
     sys_thread_free(core->thread);
     sys_cond_free(core->cond);
     sys_mutex_free(core->lock);
