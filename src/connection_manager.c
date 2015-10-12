7a2ef9f9704ada94502c603abe85fc2fa571336e
ganeshmurthy <gmurthy@redhat.com>
2015-10-12 17:16:42 -0400

    DISPATCH-181 - Removed password field from the ssl_attributes array so that the ssl->enabled will not be set to true

--- src/connection_manager.c
+++ src/connection_manager.c
@@ -71,7 +71,7 @@ static bool has_attrs(qd_entity_t *entity, const char **attributes, int n) {
 }
 
 static const char *ssl_attributes[] = {
-  "certDb", "certFile", "keyFile", "passwordFile", "password"
+  "certDb", "certFile", "keyFile", "passwordFile"
 };
 static const int ssl_attributes_count = sizeof(ssl_attributes)/sizeof(ssl_attributes[0]);
 
