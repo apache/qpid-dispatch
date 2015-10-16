8353ac674a7150a1bbfa63c912ad9689a492e84a
Chuck Rolke <crolke@redhat.com>
2015-10-16 13:59:00 -0400

    DISPATCH-166: Configure SASL EXTERNAL for SSL tests regardless of system /etc/sasl2/qdrouterd.conf settings. Add new tests that prove SSL connection failure when EXTERNAL is absent.

--- tests/system_tests_qdstat.py
+++ tests/system_tests_qdstat.py
@@ -82,10 +82,17 @@ try:
         def ssl_file(name):
             return os.path.join(system_test.DIR, 'ssl_certs', name)
 
+        @staticmethod
+        def sasl_path():
+            return os.path.join(system_test.DIR, 'sasl_configs')
+
         @classmethod
         def setUpClass(cls):
             super(QdstatSslTest, cls).setUpClass()
             config = system_test.Qdrouterd.Config([
+                ('container', {'containerName': 'QdstatSslTest',
+                               'saslConfigPath': cls.sasl_path(),
+                               'saslConfigName': 'tests-mech-EXTERNAL'}),
                 ('ssl-profile', {'name': 'server-ssl',
                                  'cert-db': cls.ssl_file('ca-certificate.pem'),
                                  'cert-file': cls.ssl_file('server-certificate.pem'),
@@ -201,6 +208,86 @@ try:
 
         def test_ssl_bad_trustfile_to_unsecured(self):
             self.ssl_test_bad('unsecured_s', ['bad_trustfile'])
+
+except SSLUnavailable:
+    class QdstatSslTest(system_test.TestCase):
+        def test_skip(self):
+            self.skipTest("Proton SSL support unavailable.")
+
+
+try:
+    SSLDomain(SSLDomain.MODE_CLIENT)
+    class QdstatSslNoExternalTest(system_test.TestCase):
+        """Test qdstat can't connect without sasl_mech EXTERNAL"""
+
+        @staticmethod
+        def ssl_file(name):
+            return os.path.join(system_test.DIR, 'ssl_certs', name)
+
+        @staticmethod
+        def sasl_path():
+            return os.path.join(system_test.DIR, 'sasl_configs')
+
+        @classmethod
+        def setUpClass(cls):
+            super(QdstatSslNoExternalTest, cls).setUpClass()
+            config = system_test.Qdrouterd.Config([
+                ('container', {'containerName': 'QdstatSslTest',
+                               'saslConfigPath': cls.sasl_path(),
+                               'saslConfigName': 'tests-mech-NOEXTERNAL'}),
+                ('ssl-profile', {'name': 'server-ssl',
+                                 'cert-db': cls.ssl_file('ca-certificate.pem'),
+                                 'cert-file': cls.ssl_file('server-certificate.pem'),
+                                 'key-file': cls.ssl_file('server-private-key.pem'),
+                                 'password': 'server-password'}),
+                ('listener', {'port': cls.tester.get_port()}),
+                ('listener', {'port': cls.tester.get_port(), 'ssl-profile': 'server-ssl', 'authenticatePeer': 'no', 'requireSsl': 'yes'}),
+                ('listener', {'port': cls.tester.get_port(), 'ssl-profile': 'server-ssl', 'authenticatePeer': 'no', 'requireSsl': 'no'}),
+                ('listener', {'port': cls.tester.get_port(), 'ssl-profile': 'server-ssl', 'authenticatePeer': 'yes', 'requireSsl': 'yes',
+                              'saslMechanisms': 'EXTERNAL'})
+            ])
+            cls.router = cls.tester.qdrouterd('test-router', config)
+
+        def run_qdstat(self, args, regexp=None, address=None):
+            p = self.popen(
+                ['qdstat', '--bus', str(address or self.router.addresses[0]), '--timeout', str(system_test.TIMEOUT) ] + args,
+                name='qdstat-'+self.id(), stdout=PIPE, expect=None)
+            out = p.communicate()[0]
+            assert p.returncode == 0, \
+                "qdstat exit status %s, output:\n%s" % (p.returncode, out)
+            if regexp: assert re.search(regexp, out, re.I), "Can't find '%s' in '%s'" % (regexp, out)
+            return out
+
+        def ssl_test(self, url_name, arg_names):
+            """Run simple SSL connection test with supplied parameters.
+            See test_ssl_* below.
+            """
+            args = dict(
+                trustfile = ['--ssl-trustfile', self.ssl_file('ca-certificate.pem')],
+                bad_trustfile = ['--ssl-trustfile', self.ssl_file('bad-ca-certificate.pem')],
+                client_cert = ['--ssl-certificate', self.ssl_file('client-certificate.pem')],
+                client_key = ['--ssl-key', self.ssl_file('client-private-key.pem')],
+                client_pass = ['--ssl-password', 'client-password'])
+            args['client_cert_all'] = args['client_cert'] + args['client_key'] + args['client_pass']
+
+            addrs = [self.router.addresses[i] for i in xrange(4)];
+            urls = dict(zip(['none', 'strict', 'unsecured', 'auth'], addrs) +
+                        zip(['none_s', 'strict_s', 'unsecured_s', 'auth_s'],
+                            (Url(a, scheme="amqps") for a in addrs)))
+
+            self.run_qdstat(['--general'] + sum([args[n] for n in arg_names], []),
+                            regexp=r'(?s)Router Statistics.*Mode\s*Standalone',
+                            address=str(urls[url_name]))
+
+        def ssl_test_bad(self, url_name, arg_names):
+            self.assertRaises(AssertionError, self.ssl_test, url_name, arg_names)
+
+        def test_ssl_cert_to_auth_fail_no_sasl_external(self):
+            self.ssl_test_bad('auth_s', ['client_cert_all'])
+
+        def test_ssl_trustfile_cert_to_auth_fail_no_sasl_external(self):
+            self.ssl_test_bad('auth_s', ['trustfile', 'client_cert_all'])
+
 
 except SSLUnavailable:
     class QdstatSslTest(system_test.TestCase):
