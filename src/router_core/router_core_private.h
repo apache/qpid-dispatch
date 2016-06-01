1a10ab5bb0b4c92137fcd45e62619de7146d7ae0
Ganesh Murthy <gmurthy@redhat.com>
2016-06-01 11:05:17 -0400

    DISPATCH-357 - Added a terminus_addr field to qdr_link_t which holds the complete address of the link

--- src/router_core/router_core_private.h
+++ src/router_core/router_core_private.h
@@ -249,6 +249,7 @@ struct qdr_link_t {
     qd_link_type_t           link_type;
     qd_direction_t           link_direction;
     char                    *name;
+    char                    *terminus_addr;      /// CAUTION - The terminus_addr holds the full address of the link route. Used only for link routes
     int                      detach_count;       ///< 0, 1, or 2 depending on the state of the lifecycle
     qdr_address_t           *owning_addr;        ///< [ref] Address record that owns this link
     qdr_link_t              *connected_link;     ///< [ref] If this is a link-route, reference the connected link
@@ -592,6 +593,7 @@ void qdr_link_issue_credit_CT(qdr_core_t *core, qdr_link_t *link, int credit);
 void qdr_addr_start_inlinks_CT(qdr_core_t *core, qdr_address_t *addr);
 void qdr_delivery_push_CT(qdr_core_t *core, qdr_delivery_t *dlv);
 void qdr_delivery_free(qdr_delivery_t *delivery);
+void qdr_link_free(qdr_link_t *link);
 void qdr_delivery_release_CT(qdr_core_t *core, qdr_delivery_t *delivery);
 bool qdr_delivery_settled_CT(qdr_core_t *core, qdr_delivery_t *delivery);
 void qdr_agent_enqueue_response_CT(qdr_core_t *core, qdr_query_t *query);
