5cc501dc1e134902fa6e69d0c8b83879e808a6fd
Ernest Allen <eallen@redhat.com>
2017-06-29 12:40:31 -0400

    DISPATCH-795 Sort entity names on Schema page

--- console/stand-alone/plugin/js/qdrSchema.js
+++ console/stand-alone/plugin/js/qdrSchema.js
@@ -21,57 +21,61 @@ under the License.
  */
 var QDR = (function (QDR) {
 
-    QDR.module.controller("QDR.SchemaController", ['$scope', '$location', 'QDRService', function($scope, $location, QDRService) {
-		if (!QDRService.connected) {
-			QDRService.redirectWhenConnected("schema")
-			return;
-		}
-		// we are currently connected. setup a handler to get notified if we are ever disconnected
-		QDRService.addDisconnectAction( function () {
-			QDRService.redirectWhenConnected("schema")
-			$scope.$apply();
-		})
+  QDR.module.controller("QDR.SchemaController", ['$scope', '$location', '$timeout', 'QDRService', function($scope, $location, $timeout, QDRService) {
+    if (!QDRService.connected) {
+      QDRService.redirectWhenConnected("schema")
+      return;
+    }
+    var onDisconnect = function () {
+      $timeout( function () {QDRService.redirectWhenConnected("schema")})
+    }
+    // we are currently connected. setup a handler to get notified if we are ever disconnected
+    QDRService.addDisconnectAction( onDisconnect )
 
-        var keys2kids = function (tree, obj) {
-
-			if (obj === Object(obj)) {
-				tree.children = []
-	            for (var key in obj) {
-	                var kid = {title: key}
-	                if (obj[key] === Object(obj[key])) {
-	                    kid.isFolder = true
-	                    keys2kids(kid, obj[key])
-	                } else {
-						kid.title += (': ' + JSON.stringify(obj[key],null,2))
-	                }
-	                tree.children.push(kid)
-	            }
-			}
+    var keys2kids = function (tree, obj) {
+      if (obj === Object(obj)) {
+        tree.children = []
+        var keys = Object.keys(obj).sort()
+        for (var i=0; i<keys.length; ++i) {
+          var key = keys[i];
+          var kid = {title: key}
+          if (obj[key] === Object(obj[key])) {
+              kid.isFolder = true
+              keys2kids(kid, obj[key])
+          } else {
+            kid.title += (': ' + JSON.stringify(obj[key],null,2))
+          }
+          tree.children.push(kid)
         }
+      }
+    }
 
-		var tree = []
-		for (var key in QDRService.schema) {
-			var kid = {title: key}
+    var tree = []
+    for (var key in QDRService.schema) {
+      var kid = {title: key}
       kid.isFolder = true
-			var val = QDRService.schema[key]
-			if (val === Object(val))
-				keys2kids(kid, val)
-			else
-				kid.title += (': ' + JSON.stringify(val,null,2))
+      var val = QDRService.schema[key]
+      if (val === Object(val))
+        keys2kids(kid, val)
+      else
+        kid.title += (': ' + JSON.stringify(val,null,2))
 
-			tree.push(kid);
-		}
-        $('#schema').dynatree({
-			minExpandLevel: 2,
+      tree.push(kid);
+    }
+    $('#schema').dynatree({
+      minExpandLevel: 2,
       classNames: {
         expander: 'fa-angle',
         connector: 'dynatree-no-connector'
-        },
+      },
       children: tree
-        })
+    })
 
+      $scope.$on("$destroy", function(event) {
+        QDRService.delDisconnectAction( onDisconnect )
+      });
 
-    }]);
+  }]);
 
     return QDR;
 }(QDR || {}));
