4698f2fe226506d78d39bb31cfbfa853d876ad9a
Chuck Rolke <crolke@redhat.com>
2017-06-30 16:57:17 -0400

    DISPATCH-760: Add new annotation parser

--- src/iterator.c
+++ src/iterator.c
@@ -899,3 +899,13 @@ bool qd_iterator_next_segment(qd_iterator_t *iter, uint32_t *hash)
 
     return true;
 }
+
+
+void qd_iterator_get_view_cursor(
+    const qd_iterator_t   *iter,
+    qd_iterator_pointer_t *ptr)
+{
+    ptr->buffer    = iter->view_pointer.buffer;
+    ptr->cursor    = iter->view_pointer.cursor;
+    ptr->remaining = iter->view_pointer.remaining;
+}
