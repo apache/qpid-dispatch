f35ce332e620c9db18ff8f3cc9aa2c695adb9909
Ganesh Murthy <gmurthy@redhat.com>
2017-07-31 13:13:47 -0400

    DISPATCH-779 - Issue credit for multicast address when no receiver is connected

--- src/router_core/router_core_private.h
+++ src/router_core/router_core_private.h
@@ -728,7 +728,7 @@ void qdr_post_link_lost_CT(qdr_core_t *core, int link_maskbit);
 
 void qdr_post_general_work_CT(qdr_core_t *core, qdr_general_work_t *work);
 void qdr_check_addr_CT(qdr_core_t *core, qdr_address_t *addr, bool was_local);
-
+bool qdr_is_addr_treatment_multicast(qdr_address_t *addr);
 qdr_delivery_t *qdr_forward_new_delivery_CT(qdr_core_t *core, qdr_delivery_t *peer, qdr_link_t *link, qd_message_t *msg);
 void qdr_forward_deliver_CT(qdr_core_t *core, qdr_link_t *link, qdr_delivery_t *dlv);
 void qdr_connection_activate_CT(qdr_core_t *core, qdr_connection_t *conn);
