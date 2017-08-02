2c80f10877c2837af48244ff0315a12f7042eda3
Kenneth Giusti <kgiusti@apache.org>
2017-08-02 12:21:33 -0400

    DISPATCH-731: add functional tests

--- tests/system_tests_qdmanage.py
+++ tests/system_tests_qdmanage.py
@@ -351,7 +351,59 @@ class QdmanageTest(TestCase):
                          ' name=' + ssl_profile_name + ' certDb=' + self.ssl_file('ca-certificate.pem')
         output = json.loads(self.run_qdmanage(ssl_create_command))
         self.assertEqual(output['name'], ssl_profile_name)
-        self.run_qdmanage('DELETE --type=sslProfile --name=' + ssl_profile_name)
+        self.run_qdmanage('DELETE --type=sslProfile --name=' +
+        ssl_profile_name)
+
+    def test_create_delete_address_pattern(self):
+        config = [('mercury.*.earth.#', 'closest'),
+                  ('*/mars/*/#', 'multicast'),
+                  ('*.mercury', 'closest'),
+                  ('*/#/pluto', 'multicast')]
+        long_type = 'org.apache.qpid.dispatch.router.config.address'
+
+        # add patterns:
+        pcount = 0
+        for p in config:
+            query_command = 'CREATE --type=' + long_type + \
+                                             ' pattern=' + p[0] + \
+                                             ' distribution=' + p[1] + \
+                                             ' name=Pattern' + str(pcount)
+            self.run_qdmanage(query_command)
+            pcount += 1
+
+        # verify correctly added:
+        query_command = 'QUERY --type=' + long_type
+        output = json.loads(self.run_qdmanage(query_command))
+        total = len(output)
+
+        pcount = 0
+        for o in output:
+            pattern = o.get('pattern')
+            if pattern is not None:
+                for p in config:
+                    if p[0] == pattern:
+                        pcount += 1
+                        self.assertEqual(p[1], o.get('distribution'))
+        self.assertEqual(pcount, len(config))
+
+        # delete
+        pcount = 0
+        for p in config:
+            query_command = 'DELETE --type=' + long_type + \
+                                             ' --name=Pattern' + str(pcount)
+            self.run_qdmanage(query_command)
+            pcount += 1
+
+        # verify deleted:
+        query_command = 'QUERY --type=' + long_type
+        output = json.loads(self.run_qdmanage(query_command))
+        self.assertEqual(len(output), total - len(config))
+        for o in output:
+            pattern = o.get('pattern')
+            if pattern is not None:
+                for p in config:
+                    self.assertNotEqual(p[0], pattern)
+
 
 if __name__ == '__main__':
     unittest.main(main_module())
