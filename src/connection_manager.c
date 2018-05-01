4fb04f463c6b0f9e1b8b0d9b1b9f2dfa87df12b7
Ganesh Murthy <gmurthy@redhat.com>
2018-05-01 14:57:56 -0400

    DISPATCH-986 - Coverity fixes. This closes #297

--- src/connection_manager.c
+++ src/connection_manager.c
@@ -595,6 +595,9 @@ qd_config_sasl_plugin_t *qd_dispatch_configure_sasl_plugin(qd_dispatch_t *qd, qd
         }
     }
 
+    free(auth_host);
+    free(auth_port);
+
     if (!sasl_plugin->auth_service) {
         sasl_plugin->auth_service               = qd_entity_opt_string(entity, "authService", 0); CHECK();
         qd_log(cm->log_source, QD_LOG_WARNING, "Attribute authService of entity authServicePlugin has been deprecated. Use host and port instead.");
