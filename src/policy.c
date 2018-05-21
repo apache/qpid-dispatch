027879709f5ab20cfc4af23b7c15f379d05d34fa
Chuck Rolke <crolke@redhat.com>
2018-05-21 10:42:53 -0400

    DISPATCH-990: Fix release build.
    A variable was used only in an assert and was unused in release.

--- src/policy.c
+++ src/policy.c
@@ -840,7 +840,9 @@ bool qd_policy_host_pattern_add(qd_policy_t *policy, char *hostPattern)
     void *oldp = qd_parse_tree_add_pattern_str(policy->hostname_tree, hostPattern, hostPattern);
     if (oldp) {
         void *recovered = qd_parse_tree_add_pattern_str(policy->hostname_tree, (char *)oldp, oldp);
-        assert (recovered);
+        if (!recovered) {
+            assert (recovered);
+        }
     }
     sys_mutex_unlock(policy->tree_lock);
     if (oldp)
