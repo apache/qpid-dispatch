081db59ef6774822c834a1a4e7cc0195af4aea72
Ganesh Murthy <gmurthy@redhat.com>
2018-05-30 10:54:03 -0400

    DISPATCH-1008 - Added additional unit test

--- tests/system_tests_handle_failover.py
+++ tests/system_tests_handle_failover.py
@@ -29,13 +29,14 @@ class FailoverTest(TestCase):
     inter_router_port = None
 
     @classmethod
+    def router(cls, name, config):
+        config = Qdrouterd.Config(config)
+
+        cls.routers.append(cls.tester.qdrouterd(name, config, wait=True))
+
+    @classmethod
     def setUpClass(cls):
         super(FailoverTest, cls).setUpClass()
-
-        def router(name, config):
-            config = Qdrouterd.Config(config)
-
-            cls.routers.append(cls.tester.qdrouterd(name, config, wait=True))
 
         cls.routers = []
 
@@ -58,7 +59,7 @@ class FailoverTest(TestCase):
         # The suite of tests determine if the router receiving this open frame stores it properly and if the
         # original connection goes down, check that the router is trying to make connections to the failover urls.
         #
-        router('B', [
+        FailoverTest.router('B', [
                         ('router', {'mode': 'interior', 'id': 'B'}),
                         ('listener', {'host': '0.0.0.0', 'role': 'inter-router', 'port': cls.inter_router_port,
                                       'failoverUrls': cls.failover_list}),
@@ -66,7 +67,7 @@ class FailoverTest(TestCase):
                         ]
               )
 
-        router('A',
+        FailoverTest.router('A',
                     [
                         ('router', {'mode': 'interior', 'id': 'A'}),
                         ('listener', {'host': '0.0.0.0', 'role': 'normal', 'port': cls.tester.get_port()}),
@@ -75,7 +76,7 @@ class FailoverTest(TestCase):
                     ]
                )
 
-        router('C', [
+        FailoverTest.router('C', [
                             ('router', {'mode': 'interior', 'id': 'C'}),
                             ('listener', {'host': '0.0.0.0', 'role': 'inter-router', 'port': cls.backup_port}),
                             ('listener', {'host': '0.0.0.0', 'role': 'normal', 'port': cls.tester.get_port()}),
@@ -116,7 +117,7 @@ class FailoverTest(TestCase):
         if regexp: assert re.search(regexp, out, re.I), "Can't find '%s' in '%s'" % (regexp, out)
         return out
 
-    def test_connector_has_failover_list(self):
+    def test_1_connector_has_failover_list(self):
         """
         Makes a qdmanage connector query and checks if Router A is storing the failover information received from
         Router B.
@@ -125,15 +126,16 @@ class FailoverTest(TestCase):
         long_type = 'org.apache.qpid.dispatch.connector'
         query_command = 'QUERY --type=' + long_type
         output = json.loads(self.run_qdmanage(query_command))
-        self.assertIn(FailoverTest.failover_list, output[0]['failoverUrls'])
+        self.assertEqual("amqp://127.0.0.1:" + str(FailoverTest.inter_router_port) + ", " + FailoverTest.failover_list,
+                         output[0]['failoverUrls'])
 
-    def schedule_failover_test(self):
+    def schedule_B_to_C_failover_test(self):
         if self.attempts < self.max_attempts:
             if not self.success:
-                Timer(self.timer_delay, self.check_connector).start()
+                Timer(self.timer_delay, self.check_C_connector).start()
                 self.attempts += 1
 
-    def check_connector(self):
+    def check_C_connector(self):
         # Router A should now try to connect to Router C. Router C does NOT have failoverUrls.
         # Query Router A which previously had failoverUrls in its connector (because Router B sent it failoverUrls)
         # does not have it anymore.
@@ -146,7 +148,7 @@ class FailoverTest(TestCase):
         if output[0].get('failoverUrls') == expected:
             self.success = True
         else:
-            self.schedule_failover_test()
+            self.schedule_B_to_C_failover_test()
 
     def can_terminate(self):
         if self.attempts == self.max_attempts:
@@ -157,7 +159,7 @@ class FailoverTest(TestCase):
 
         return False
 
-    def test_remove_router_B(self):
+    def test_2_remove_router_B(self):
         # First make sure there are no inter-router connections on router C
         outs = self.run_qdstat(['--connections'], address=self.routers[2].addresses[1])
 
@@ -167,8 +169,8 @@ class FailoverTest(TestCase):
         # Kill the router B
         FailoverTest.routers[0].teardown()
 
-        # Schedule a test to make sure that the failover url is empty
-        self.schedule_failover_test()
+        # Schedule a test to make sure that the failover url is available
+        self.schedule_B_to_C_failover_test()
 
         while not self.can_terminate():
             pass
@@ -178,6 +180,52 @@ class FailoverTest(TestCase):
         # Since router B has been killed, router A should now try to connect to a listener on router C.
         # Use qdstat to connect to router C and determine that there is an inter-router connection with router A.
         self.run_qdstat(['--connections'], regexp=r'A.*inter-router.*', address=self.routers[2].addresses[1])
+
+    def schedule_C_to_B_failover_test(self):
+        if self.attempts < self.max_attempts:
+            if not self.success:
+                Timer(self.timer_delay, self.check_B_connector).start()
+                self.attempts += 1
+
+    def check_B_connector(self):
+        # Router A should now try to connect to Router B again since we killed Router C.
+        long_type = 'org.apache.qpid.dispatch.connector'
+        query_command = 'QUERY --type=' + long_type
+        output = json.loads(self.run_qdmanage(query_command, address=self.routers[1].addresses[0]))
+
+        expected = "amqp://127.0.0.1:" + str(FailoverTest.inter_router_port) + ", " + FailoverTest.failover_list
+
+        if output[0].get('failoverUrls') == expected:
+            self.success = True
+        else:
+            self.schedule_C_to_B_failover_test()
+
+    def test_3_reinstate_router_B(self):
+        FailoverTest.router('B', [
+                        ('router', {'mode': 'interior', 'id': 'B'}),
+                        ('listener', {'host': '0.0.0.0', 'role': 'inter-router', 'port': FailoverTest.inter_router_port,
+                                      'failoverUrls': FailoverTest.failover_list}),
+                        ('listener', {'host': '0.0.0.0', 'role': 'normal', 'port': FailoverTest.tester.get_port()}),
+                        ]
+              )
+
+        FailoverTest.routers[3].wait_ready()
+
+        # Kill the router C.
+        # Now since Router B is up and running, router A should try to re-connect to Router B.
+        # This will prove that the router A is preserving the original connector information specified in its config.
+        FailoverTest.routers[2].teardown()
+
+        self.success = False
+        self.attempts = 0
+
+        # Schedule a test to make sure that the failover url is available
+        self.schedule_C_to_B_failover_test()
+
+        while not self.can_terminate():
+            pass
+
+        self.assertTrue(self.success)
 
 
 if __name__ == '__main__':
