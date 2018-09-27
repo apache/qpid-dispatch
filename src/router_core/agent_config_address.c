d1d6e92df2d3adbba5d86e650e0d44a0ffc0b33e
Kenneth Giusti <kgiusti@apache.org>
2018-09-27 13:22:51 -0400

    DISPATCH-1131: add better validation of address patterns and prefixes
    This moves the address pattern/prefix validation to a common function
    that can be share by both link routes add configured addresses.
    
    This closes #388

--- src/router_core/agent_config_address.c
+++ src/router_core/agent_config_address.c
@@ -303,8 +303,8 @@ void qdra_config_address_create_CT(qdr_core_t         *core,
                                    qdr_query_t        *query,
                                    qd_parsed_field_t  *in_body)
 {
+    char *pattern = NULL;
     qd_iterator_t *iter = NULL;
-    char *buf = NULL;
 
     while (true) {
         //
@@ -345,38 +345,28 @@ void qdra_config_address_create_CT(qdr_core_t         *core,
         // Either a prefix or a pattern field is mandatory.  Prefix and pattern
         // are mutually exclusive. Fail if either both or none are given.
         //
-        if ((!prefix_field && !pattern_field) || (prefix_field && pattern_field)) {
+        const char *msg = NULL;
+        if (!prefix_field && !pattern_field) {
+            msg = "Either a 'prefix' or 'pattern' attribute must be provided";
+        } else if (prefix_field && pattern_field) {
+            msg = "Cannot specify both a 'prefix' and a 'pattern' attribute";
+        }
+        if (msg) {
             query->status = QD_AMQP_BAD_REQUEST;
-            query->status.description = prefix_field ? "cannot specify both prefix and pattern"
-                : "a prefix or pattern field is mandatory";
+            query->status.description = msg;
             qd_log(core->agent_log, QD_LOG_ERROR, "Error performing CREATE of %s: %s", CONFIG_ADDRESS_TYPE, query->status.description);
             break;
         }
 
-        // strip leading and trailing token separators
-        qd_iterator_t *tmp = qd_iterator_dup(qd_parse_raw(prefix_field ? prefix_field : pattern_field));
-        const int len = qd_iterator_length(tmp);
-        buf = malloc(len + 3);   // +'/#' if needed
-        char *pattern = qd_iterator_strncpy(tmp, buf, len + 1);
-        qd_iterator_free(tmp);
-
-        // note: see parse_tree.c for acceptable separator characters
-        while (*pattern && strchr("./", *pattern))
-            pattern++;
-        while (*pattern && strchr("./", pattern[strlen(pattern) - 1]))
-            pattern[strlen(pattern) - 1] = '\0';
-
-        if (!*pattern) {
+        // validate the pattern/prefix, add "/#" if prefix
+        pattern = qdra_config_address_validate_pattern_CT((prefix_field) ? prefix_field : pattern_field,
+                                                          !!prefix_field,
+                                                          &msg);
+        if (!pattern) {
             query->status = QD_AMQP_BAD_REQUEST;
-            query->status.description = "invalid address pattern";
+            query->status.description = msg;
             qd_log(core->agent_log, QD_LOG_ERROR, "Error performing CREATE of %s: %s", CONFIG_ADDRESS_TYPE, query->status.description);
             break;
-        }
-
-        if (prefix_field) {
-            // convert the old prefix value into a pattern by appending '/#'
-            // (match zero or more tokens)
-            strcat(pattern, "/#");
         }
 
         iter = qd_iterator_string(pattern, ITER_VIEW_ALL);
@@ -427,8 +417,9 @@ void qdra_config_address_create_CT(qdr_core_t         *core,
         addr->treatment = qdra_address_treatment_CT(distrib_field);
         addr->in_phase  = in_phase;
         addr->out_phase = out_phase;
-        addr->pattern   = (char *) qd_iterator_copy(iter);
         addr->is_prefix = !!prefix_field;
+        addr->pattern   = pattern;
+        pattern = 0;
 
         qd_iterator_reset_view(iter, ITER_VIEW_ALL);
         qd_parse_tree_add_pattern(core->addr_parse_tree, iter, addr);
@@ -462,10 +453,8 @@ void qdra_config_address_create_CT(qdr_core_t         *core,
     } else
         qdr_query_free(query);
 
-    if (buf) {
-        free(buf);
-        qd_iterator_free(iter);
-    }
+    free(pattern);
+    qd_iterator_free(iter);
 }
 
 
@@ -522,4 +511,62 @@ void qdra_config_address_get_CT(qdr_core_t    *core,
     //
     qdr_agent_enqueue_response_CT(core, query);
 
+}
+
+
+// given an address pattern parsed field, validate it and convert it to a string
+char *qdra_config_address_validate_pattern_CT(qd_parsed_field_t *pattern_field,
+                                              bool is_prefix,
+                                              const char **error)
+{
+    char *buf = NULL;
+    char *pattern = NULL;
+    uint8_t tag = qd_parse_tag(pattern_field);
+    qd_iterator_t *p_iter = qd_parse_raw(pattern_field);
+    int len = qd_iterator_length(p_iter);
+    *error = NULL;
+
+    if ((tag != QD_AMQP_STR8_UTF8 && tag != QD_AMQP_STR32_UTF8)
+        || len == 0)
+    {
+        *error = ((is_prefix)
+                  ? "Prefix must be a non-empty string type"
+                  : "Pattern must be a non-empty string type");
+        goto exit;
+    }
+
+    buf = (char *)qd_iterator_copy(p_iter);
+    char *begin = buf;
+    // strip leading token separators
+    // note: see parse_tree.c for acceptable separator characters
+    while (*begin && strchr("./", *begin))
+        begin++;
+
+    // strip trailing separators
+    while (*begin) {
+        char *end = &begin[strlen(begin) - 1];
+        if (!strchr("./", *end))
+            break;
+        *end = 0;
+    }
+
+    if (*begin == 0) {
+        *error = ((is_prefix)
+                  ? "Prefix invalid - no tokens"
+                  : "Pattern invalid - no tokens");
+        goto exit;
+    }
+
+    if (is_prefix) {
+        // convert a prefix match into a valid pattern by appending "/#"
+        pattern = malloc(strlen(begin) + 3);
+        strcpy(pattern, begin);
+        strcat(pattern, "/#");
+    } else {
+        pattern = strdup(begin);
+    }
+
+exit:
+    free(buf);
+    return pattern;
 }
