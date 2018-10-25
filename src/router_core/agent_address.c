04d2aa0fbda7621687f72dd5a8070e3f7dc85309
Ernest Allen <eallen@redhat.com>
2018-10-25 12:07:36 -0400

    DISPATCH-1147 Expose address priority

--- src/router_core/agent_address.c
+++ src/router_core/agent_address.c
@@ -39,6 +39,7 @@
 #define QDR_ADDRESS_DELIVERIES_INGRESS_ROUTE_CONTAINER 16
 #define QDR_ADDRESS_TRANSIT_OUTSTANDING                17
 #define QDR_ADDRESS_TRACKED_DELIVERIES                 18
+#define QDR_ADDRESS_PRIORITY                           19
 
 const char *qdr_address_columns[] =
     {"name",
@@ -60,6 +61,7 @@ const char *qdr_address_columns[] =
      "deliveriesIngressRouteContainer",
      "transitOutstanding",
      "trackedDeliveries",
+     "priority",
      0};
 
 
@@ -165,6 +167,10 @@ static void qdr_insert_address_columns_CT(qdr_core_t          *core,
 
     case QDR_ADDRESS_TRACKED_DELIVERIES:
         qd_compose_insert_long(body, addr->tracked_deliveries);
+        break;
+
+    case QDR_ADDRESS_PRIORITY:
+        qd_compose_insert_int(body, addr->priority);
         break;
 
     default:
--- src/router_core/agent_address.h
+++ src/router_core/agent_address.h
@@ -31,7 +31,7 @@ void qdra_address_get_CT(qdr_core_t *core,
                       const char *qdr_address_columns[]);
 
 
-#define QDR_ADDRESS_COLUMN_COUNT 19
+#define QDR_ADDRESS_COLUMN_COUNT 20
 
 const char *qdr_address_columns[QDR_ADDRESS_COLUMN_COUNT + 1];
 
