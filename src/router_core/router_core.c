75a84f6e43b76cbef88f5edf1ce8fb8814858b79
Kenneth Giusti <kgiusti@apache.org>
2019-01-18 11:46:17 -0500

    DISPATCH-1248: free autolink core timers on shutdown. This closes #443

--- src/router_core/router_core.c
+++ src/router_core/router_core.c
@@ -422,6 +422,7 @@ void qdr_core_delete_auto_link(qdr_core_t *core, qdr_auto_link_t *al)
 {
     free(al->name);
     free(al->external_addr);
+    qdr_core_timer_free_CT(core, al->retry_timer);
     free_qdr_auto_link_t(al);
 }
 
