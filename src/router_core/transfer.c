9dbb4c5de6ff2c73ef84b2126e1f7aaf7a2272f8
Ganesh Murthy <gmurthy@apache.org>
2019-05-08 09:55:23 -0400

    DISPATCH-1326 - Additional fix to initialize the temp_link object. This caused a crash

--- src/router_core/transfer.c
+++ src/router_core/transfer.c
@@ -664,6 +664,7 @@ static void qdr_link_deliver_CT(qdr_core_t *core, qdr_action_t *action, bool dis
                         qdr_link_ref_t *sender_rlink = DEQ_HEAD(sender_address->rlinks);
                         if (sender_rlink) {
                             temp_rlink = new_qdr_link_ref_t();
+                            DEQ_ITEM_INIT(temp_rlink);
                             temp_rlink->link = sender_rlink->link;
                             DEQ_INSERT_TAIL(addr->rlinks, temp_rlink);
                         }
