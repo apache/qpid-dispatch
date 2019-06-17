a1bba084d24f3c8c0585a0d475454b9b646d7b25
Francesco Nigro <nigro.fra@gmail.com>
2019-06-17 10:28:07 +0200

    DISPATCH-1371 qd_alloc can reuse instances in LIFO order

--- src/alloc_pool.c
+++ src/alloc_pool.c
@@ -138,9 +138,9 @@ void *qd_alloc(qd_alloc_type_desc_t *desc, qd_alloc_pool_t **tpool)
     // list and return it.  Since everything we've touched is thread-local,
     // there is no need to acquire a lock.
     //
-    qd_alloc_item_t *item = DEQ_HEAD(pool->free_list);
+    qd_alloc_item_t *item = DEQ_TAIL(pool->free_list);
     if (item) {
-        DEQ_REMOVE_HEAD(pool->free_list);
+        DEQ_REMOVE_TAIL(pool->free_list);
 #ifdef QD_MEMORY_DEBUG
         item->desc   = desc;
         item->header = PATTERN_FRONT;
@@ -164,8 +164,8 @@ void *qd_alloc(qd_alloc_type_desc_t *desc, qd_alloc_pool_t **tpool)
         desc->stats->held_by_threads += desc->config->transfer_batch_size;
 #endif
         for (idx = 0; idx < desc->config->transfer_batch_size; idx++) {
-            item = DEQ_HEAD(desc->global_pool->free_list);
-            DEQ_REMOVE_HEAD(desc->global_pool->free_list);
+            item = DEQ_TAIL(desc->global_pool->free_list);
+            DEQ_REMOVE_TAIL(desc->global_pool->free_list);
             DEQ_INSERT_TAIL(pool->free_list, item);
         }
     } else {
@@ -192,9 +192,9 @@ void *qd_alloc(qd_alloc_type_desc_t *desc, qd_alloc_pool_t **tpool)
     }
     sys_mutex_unlock(desc->lock);
 
-    item = DEQ_HEAD(pool->free_list);
+    item = DEQ_TAIL(pool->free_list);
     if (item) {
-        DEQ_REMOVE_HEAD(pool->free_list);
+        DEQ_REMOVE_TAIL(pool->free_list);
 #ifdef QD_MEMORY_DEBUG
         item->desc = desc;
         item->header = PATTERN_FRONT;
@@ -256,8 +256,8 @@ void qd_dealloc(qd_alloc_type_desc_t *desc, qd_alloc_pool_t **tpool, char *p)
     desc->stats->held_by_threads -= desc->config->transfer_batch_size;
 #endif
     for (idx = 0; idx < desc->config->transfer_batch_size; idx++) {
-        item = DEQ_HEAD(pool->free_list);
-        DEQ_REMOVE_HEAD(pool->free_list);
+        item = DEQ_TAIL(pool->free_list);
+        DEQ_REMOVE_TAIL(pool->free_list);
         DEQ_INSERT_TAIL(desc->global_pool->free_list, item);
     }
 
@@ -267,8 +267,8 @@ void qd_dealloc(qd_alloc_type_desc_t *desc, qd_alloc_pool_t **tpool, char *p)
     //
     if (desc->config->global_free_list_max != 0) {
         while (DEQ_SIZE(desc->global_pool->free_list) > desc->config->global_free_list_max) {
-            item = DEQ_HEAD(desc->global_pool->free_list);
-            DEQ_REMOVE_HEAD(desc->global_pool->free_list);
+            item = DEQ_TAIL(desc->global_pool->free_list);
+            DEQ_REMOVE_TAIL(desc->global_pool->free_list);
             free(item);
 #if QD_MEMORY_STATS
             desc->stats->total_free_to_heap++;
@@ -314,7 +314,7 @@ void qd_alloc_finalize(void)
     //
 
     qd_alloc_item_t *item;
-    qd_alloc_type_t *type_item = DEQ_HEAD(type_list);
+    qd_alloc_type_t *type_item = DEQ_TAIL(type_list);
 
     FILE *dump_file = 0;
     if (debug_dump) {
@@ -329,14 +329,14 @@ void qd_alloc_finalize(void)
         //
         // Reclaim the items on the global free pool
         //
-        item = DEQ_HEAD(desc->global_pool->free_list);
+        item = DEQ_TAIL(desc->global_pool->free_list);
         while (item) {
-            DEQ_REMOVE_HEAD(desc->global_pool->free_list);
+            DEQ_REMOVE_TAIL(desc->global_pool->free_list);
             free(item);
 #if QD_MEMORY_STATS
             desc->stats->total_free_to_heap++;
 #endif
-            item = DEQ_HEAD(desc->global_pool->free_list);
+            item = DEQ_TAIL(desc->global_pool->free_list);
         }
         free(desc->global_pool);
         desc->global_pool = 0;
@@ -344,21 +344,21 @@ void qd_alloc_finalize(void)
         //
         // Reclaim the items on thread pools
         //
-        qd_alloc_pool_t *tpool = DEQ_HEAD(desc->tpool_list);
+        qd_alloc_pool_t *tpool = DEQ_TAIL(desc->tpool_list);
         while (tpool) {
-            item = DEQ_HEAD(tpool->free_list);
+            item = DEQ_TAIL(tpool->free_list);
             while (item) {
-                DEQ_REMOVE_HEAD(tpool->free_list);
+                DEQ_REMOVE_TAIL(tpool->free_list);
                 free(item);
 #if QD_MEMORY_STATS
                 desc->stats->total_free_to_heap++;
 #endif
-                item = DEQ_HEAD(tpool->free_list);
+                item = DEQ_TAIL(tpool->free_list);
             }
 
-            DEQ_REMOVE_HEAD(desc->tpool_list);
+            DEQ_REMOVE_TAIL(desc->tpool_list);
             free(tpool);
-            tpool = DEQ_HEAD(desc->tpool_list);
+            tpool = DEQ_TAIL(desc->tpool_list);
         }
 
         //
@@ -382,9 +382,9 @@ void qd_alloc_finalize(void)
         desc->lock = 0;
         desc->trailer = 0;
 
-        DEQ_REMOVE_HEAD(type_list);
+        DEQ_REMOVE_TAIL(type_list);
         free(type_item);
-        type_item = DEQ_HEAD(type_list);
+        type_item = DEQ_TAIL(type_list);
     }
 
     sys_mutex_free(init_lock);
