589c54c65a07f7bc8c003a879c9518f05db9a7e0
Kenneth Giusti <kgiusti@apache.org>
2019-09-03 16:14:40 -0400

    DISPATCH-1337: remove spurious assert(false)

--- src/router_core/transfer.c
+++ src/router_core/transfer.c
@@ -726,7 +726,6 @@ static void qdr_link_deliver_CT(qdr_core_t *core, qdr_action_t *action, bool dis
             free_qdr_link_ref_t(temp_rlink);
         }
     } else {
-        assert(false);
         //
         // Take the action reference and use it for undelivered.  Don't decref/incref.
         //
