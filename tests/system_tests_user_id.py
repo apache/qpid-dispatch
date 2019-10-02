a6a170c359a6a0c0015426cd5fdb122892ab1791
Ganesh Murthy <gmurthy@apache.org>
2019-10-02 16:53:22 -0400

    Revert "NO-JIRA - Added tests to make sure you can specify env: and literal: prefixes in the password file"
    This reverts commit 38c1ec6d4b02964f02439aa46cda85f54aaec085.

--- tests/system_tests_user_id.py
+++ tests/system_tests_user_id.py
@@ -38,8 +38,6 @@ class QdSSLUseridTest(TestCase):
     def setUpClass(cls):
         super(QdSSLUseridTest, cls).setUpClass()
 
-        os.environ["TLS_SERVER_PASSWORD"] = "server-password"
-
         ssl_profile1_json = os.path.join(DIR, 'displayname_files', 'profile_names1.json')
         ssl_profile2_json = os.path.join(DIR, 'displayname_files', 'profile_names2.json')
 
@@ -125,7 +123,7 @@ class QdSSLUseridTest(TestCase):
                              'privateKeyFile': cls.ssl_file('server-private-key.pem'),
                              'uidFormat': '1x',
                              'uidNameMappingFile': ssl_profile2_json,
-                             'passwordFile': cls.ssl_file('server-password-file-literal.txt')}),
+                             'password': 'server-password'}),
 
             # All components in the uidFormat are unrecognized, pn_get_transport_user will be returned
             ('sslProfile', {'name': 'server-ssl11',
@@ -133,7 +131,7 @@ class QdSSLUseridTest(TestCase):
                              'certFile': cls.ssl_file('server-certificate.pem'),
                              'privateKeyFile': cls.ssl_file('server-private-key.pem'),
                              'uidFormat': 'abxd',
-                             'passwordFile': cls.ssl_file('server-password-file-env.txt')}),
+                             'password': 'server-password'}),
 
             ('sslProfile', {'name': 'server-ssl12',
                              'caCertFile': cls.ssl_file('ca-certificate.pem'),
