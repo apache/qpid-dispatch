1c7b947b12981de8cd3e9bbb5d0c4faad3b6e148
Kenneth Giusti <kgiusti@apache.org>
2020-03-12 14:23:17 -0400

    DISPATCH-1597: release the qd_connection in libwebsockets on transport close

--- src/http-libwebsockets.c
+++ src/http-libwebsockets.c
@@ -178,7 +178,9 @@ static int handle_events(connection_t* c) {
     }
     pn_event_t *e;
     while ((e = pn_connection_driver_next_event(&c->driver))) {
-        qd_connection_handle(c->qd_conn, e);
+        if (!qd_connection_handle(c->qd_conn, e)) {
+            c->qd_conn = 0;  // connection closed
+        }
     }
     if (pn_connection_driver_write_buffer(&c->driver).size) {
         lws_callback_on_writable(c->wsi);
