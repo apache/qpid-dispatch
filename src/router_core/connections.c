daf559fc44a6ac7d4845cd7ec6fdea30db87657d
Ganesh Murthy <gmurthy@apache.org>
2020-07-30 18:08:20 -0400

    DISPATCH-1733 - Free leaking qdr_connection_t when the router core action has been discarded

--- src/router_core/connections.c
+++ src/router_core/connections.c
@@ -1381,6 +1381,10 @@ static void qdr_connection_opened_CT(qdr_core_t *core, qdr_action_t *action, boo
         qdrc_event_conn_raise(core, QDRC_EVENT_CONN_OPENED, conn);
     }
 
+    if (discard && conn) {
+        qdr_connection_free(conn);
+    }
+
     qdr_field_free(action->args.connection.connection_label);
     qdr_field_free(action->args.connection.container_id);
 }
