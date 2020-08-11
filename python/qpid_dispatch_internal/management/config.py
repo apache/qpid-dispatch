c0fb276ecc3a193e9f03790cd3b4f910677cbe3a
Gordon Sim <gsim@redhat.com>
2020-08-11 11:32:33 +0100

    DISPATCH-1654: Initial TCP adaptor

--- python/qpid_dispatch_internal/management/config.py
+++ python/qpid_dispatch_internal/management/config.py
@@ -313,7 +313,7 @@ def configure_dispatch(dispatch, lib_handle, filename):
     for t in "sslProfile", "authServicePlugin", "listener", "connector", \
              "router.config.address", "router.config.linkRoute", "router.config.autoLink", \
              "router.config.exchange", "router.config.binding", \
-             "vhost", "httpListener", "httpConnector":
+             "vhost", "httpListener", "httpConnector", "tcpListener", "tcpConnector":
         for a in config.by_type(t):
             configure(a)
             if t == "sslProfile":
