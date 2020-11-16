e25d7bcc93dd202a69895243e238e5758c14e34c
Ganesh Murthy <gmurthy@apache.org>
2020-11-16 10:28:09 -0500

    DISPATCH-1834: Code review fix. Preserved the assertTrue but moved the 'or' check to a boolean variable

--- tests/router_policy_test.py
+++ tests/router_policy_test.py
@@ -284,8 +284,8 @@ class PolicyAppConnectionMgrTests(TestCase):
         self.assertTrue(stats.can_connect('10.10.10.10:10000', 'chuck', '10.10.10.10', diags, None, None))
         self.assertFalse(stats.can_connect('10.10.10.10:10001', 'chuck', '10.10.10.10', diags, None, None))
         self.assertTrue(len(diags) == 2)
-        self.assertIn('per user', diags[0] or 'per user' in diags[1])
-        self.assertTrue('per host' in diags[0] or 'per host' in diags[1])
+        success = 'per user' in diags[0] or 'per user' in diags[1]
+        self.assertTrue(success)
         diags = []
         self.assertTrue(stats.can_connect('10.10.10.10:10002', 'chuck', '10.10.10.10', diags, 2, 2))
         self.assertFalse(stats.can_connect('10.10.10.10:10003', 'chuck', '10.10.10.10', diags, 2, 2))
