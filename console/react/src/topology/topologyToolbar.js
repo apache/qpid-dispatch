9dfa915e6ad6bd2b9b071a54bdb1202914e06534
Jiri Danek <jdanek@redhat.com>
2021-01-18 06:05:33 +0100

    Reduce height of the Topology and Chord toolbar
    There were two <Toolbar>s nested one inside another

--- console/react/src/topology/topologyToolbar.js
+++ console/react/src/topology/topologyToolbar.js
@@ -18,7 +18,7 @@ under the License.
 */
 
 import React from "react";
-import { Toolbar, ToolbarGroup, ToolbarItem } from "@patternfly/react-core";
+import {ToolbarGroup, ToolbarItem} from "@patternfly/react-core";
 import TrafficComponent from "./trafficComponent";
 import MapComponent from "./mapComponent";
 import ArrowsComponent from "./arrowsComponent";
@@ -27,56 +27,51 @@ import DropdownPanel from "../common/dropdownPanel";
 class TopologyToolbar extends React.Component {
   render() {
     return (
-      <Toolbar
-        data-testid="topology-toolbar"
-        className="pf-l-toolbar pf-u-justify-content-space-between pf-u-mx-xl pf-u-my-md"
-      >
-        <ToolbarGroup>
-          <ToolbarItem className="pf-u-mr-md">
-            <DropdownPanel
-              title="Traffic"
-              panel={
-                <TrafficComponent
-                  addressColors={this.props.addressColors}
-                  handleChangeTrafficAnimation={this.props.handleChangeTrafficAnimation}
-                  handleChangeTrafficFlowAddress={
-                    this.props.handleChangeTrafficFlowAddress
-                  }
-                  handleHoverAddress={this.props.handleHoverAddress}
-                  dots={this.props.legendOptions.traffic.dots}
-                  congestion={this.props.legendOptions.traffic.congestion}
-                />
-              }
-            />
-          </ToolbarItem>
-          <ToolbarItem className="pf-u-mr-md">
-            <DropdownPanel
-              title="Background map"
-              panel={
-                <MapComponent
-                  mapShown={this.props.legendOptions.map.show}
-                  areaColor={this.props.mapOptions.areaColor}
-                  oceanColor={this.props.mapOptions.oceanColor}
-                  handleUpdateMapColor={this.props.handleUpdateMapColor}
-                  handleUpdateMapShown={this.props.handleUpdateMapShown}
-                />
-              }
-            />
-          </ToolbarItem>
-          <ToolbarItem className="pf-u-mr-md">
-            <DropdownPanel
-              title="Arrows"
-              panel={
-                <ArrowsComponent
-                  routerArrows={this.props.legendOptions.arrows.routerArrows}
-                  clientArrows={this.props.legendOptions.arrows.clientArrows}
-                  handleChangeArrows={this.props.handleChangeArrows}
-                />
-              }
-            />
-          </ToolbarItem>
-        </ToolbarGroup>
-      </Toolbar>
+      <ToolbarGroup>
+        <ToolbarItem data-testid="topology-toolbar" className="pf-u-mr-md">
+          <DropdownPanel
+            title="Traffic"
+            panel={
+              <TrafficComponent
+                addressColors={this.props.addressColors}
+                handleChangeTrafficAnimation={this.props.handleChangeTrafficAnimation}
+                handleChangeTrafficFlowAddress={
+                  this.props.handleChangeTrafficFlowAddress
+                }
+                handleHoverAddress={this.props.handleHoverAddress}
+                dots={this.props.legendOptions.traffic.dots}
+                congestion={this.props.legendOptions.traffic.congestion}
+              />
+            }
+          />
+        </ToolbarItem>
+        <ToolbarItem className="pf-u-mr-md">
+          <DropdownPanel
+            title="Background map"
+            panel={
+              <MapComponent
+                mapShown={this.props.legendOptions.map.show}
+                areaColor={this.props.mapOptions.areaColor}
+                oceanColor={this.props.mapOptions.oceanColor}
+                handleUpdateMapColor={this.props.handleUpdateMapColor}
+                handleUpdateMapShown={this.props.handleUpdateMapShown}
+              />
+            }
+          />
+        </ToolbarItem>
+        <ToolbarItem className="pf-u-mr-md">
+          <DropdownPanel
+            title="Arrows"
+            panel={
+              <ArrowsComponent
+                routerArrows={this.props.legendOptions.arrows.routerArrows}
+                clientArrows={this.props.legendOptions.arrows.clientArrows}
+                handleChangeArrows={this.props.handleChangeArrows}
+              />
+            }
+          />
+        </ToolbarItem>
+      </ToolbarGroup>
     );
   }
 }
