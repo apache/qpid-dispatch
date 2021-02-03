586912749fa1c191eb4160eb6aba15b0b523b832
Ganesh Murthy <gmurthy@apache.org>
2021-02-03 16:37:51 -0500

    DISPATCH-1950: Knock down the version of libnghttp2 to 1.33.0. Also added code to Findlibnghttp2.cmake to extract the version of the library from nghttp2ver.h. This closes #1017

--- cmake/Findlibnghttp2.cmake
+++ cmake/Findlibnghttp2.cmake
@@ -17,16 +17,31 @@
 # under the License.
 #
 
+# Sets LIBNGHTTP2_VERSION_STRING from nghttp2ver.h
+
 find_library(NGHTTP2_LIBRARIES
   NAMES libnghttp2 nghttp2
 )
 
 find_path(NGHTTP2_INCLUDE_DIRS
-  NAMES "nghttp2/nghttp2.h"
+  NAMES "nghttp2/nghttp2.h" "nghttp2/nghttp2ver.h"
   HINTS "${CMAKE_INSTALL_PREFIX}/include"
   PATHS "/usr/include"
 )
 
-include(FindPackageHandleStandardArgs)
+if(NGHTTP2_INCLUDE_DIRS AND EXISTS "${NGHTTP2_INCLUDE_DIRS}/nghttp2/nghttp2ver.h")
+  # Extract the version info from nghttp2ver.h and set it in LIBNGHTTP2_VERSION_STRING
+  file(STRINGS "${NGHTTP2_INCLUDE_DIRS}/nghttp2/nghttp2ver.h" libnghttp2_version_str
+    REGEX "^#define[ \t]+NGHTTP2_VERSION[ \t]+\"[^\"]+\"")
+  string(REGEX REPLACE "^#define[ \t]+NGHTTP2_VERSION[ \t]+\"([^\"]+)\".*" "\\1"
+    LIBNGHTTP2_VERSION_STRING "${libnghttp2_version_str}")
+  unset(libnghttp2_version_str)
+endif()
+
+if (LIBNGHTTP2_VERSION_STRING AND libnghttp2_FIND_VERSION AND (LIBNGHTTP2_VERSION_STRING VERSION_LESS libnghttp2_FIND_VERSION))
+  message(STATUS "Found libnghttp2 version ${LIBNGHTTP2_VERSION_STRING} but at least ${libnghttp2_FIND_VERSION} is required. http2 support is disabled")
+else()
+  include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(
-    libnghttp2 DEFAULT_MSG NGHTTP2_LIBRARIES NGHTTP2_INCLUDE_DIRS)
+    libnghttp2 DEFAULT_MSG LIBNGHTTP2_VERSION_STRING NGHTTP2_LIBRARIES NGHTTP2_INCLUDE_DIRS)
+endif()
