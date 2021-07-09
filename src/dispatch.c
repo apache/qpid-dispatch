d0a25ed11e0f9166a68aaed842571a01584ba9f7
Jiri Danek <jdanek@redhat.com>
2021-07-09 13:55:08 +0200

    DISPATCH-2051 Free dispatch struct char * attributes

--- src/dispatch.c
+++ src/dispatch.c
@@ -381,6 +381,10 @@ void qd_dispatch_free(qd_dispatch_t *qd)
     qd_python_finalize();
     qd_dispatch_set_router_id(qd, NULL);
     qd_dispatch_set_router_area(qd, NULL);
+    free(qd->timestamp_format);
+    free(qd->metadata);
+
+    free(qd);
 }
 
 
