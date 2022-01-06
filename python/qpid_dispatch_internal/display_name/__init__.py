bdb77421eed9972f59eada12bf35f840e296e5a1
Jiri Daněk <jdanek@redhat.com>
2022-01-06 13:20:11 +0100

    DISPATCH-2299 re-enable system_tests_http delete listeners (#1468)
    * DISPATCH-2299 re-enable system_tests_http delete listeners with libwebsockets >= 4.2
    
    This change also follows-up on changes in DISPATCH-2035 by that it removes
    the two remaining `configure_file` calls in CMakeLists.txt creating output
    in the `CMAKE_CURRENT_SOURCE_DIR`.
    
    * fixup, the test was not previously deleting listener on CentOS 7 and it was broken there

--- python/qpid_dispatch_internal/display_name/__init__.py
+++ python/qpid_dispatch_internal/display_name/__init__.py
@@ -16,4 +16,5 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-"Qpid Dispatch internal display name service"
+
+"""Qpid Dispatch internal display name service"""
